a3099c3078192e30055b1f2b58d6229fabbe9ffc
rogermiranda1000 <rogermirandaperez@hotmail.com>
2023-07-16 09:06:41 +0200

    Feature: generate json from one block

--- items-adapter/generate.py
+++ items-adapter/generate.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-from typing import List,Dict
+from typing import List,Dict,Tuple,Any
 import json
-from data_provider import get_mc_data_versions,get_mc_data,get_spigot_enum_json,legacy_minecraft_data_conversion
+from data_provider import get_mc_data_versions,get_mc_data,get_spigot_enum_json,get_spigot_enum,legacy_minecraft_data_conversion
 
 # It will try to convert from one version's items to the next one.
 #
@@ -43,6 +43,34 @@ def forward(old: json, new: json, legacy_conversion: Dict[str,str]) -> Dict[str,
 
 	return conversion
 
+# @param convertors list of convertors and its version, ordered from high to low.
+# 		The first version (last element) must contain a None conversion
+def generate_json_entry(material: str, convertors: List[Tuple[str,Dict[str,str]]]) -> Dict[str,Any]:
+	material = material.lower()
+
+	element = material
+	aliases = [{'name': element, 'max-version': convertors[0][0]}]
+	for i in range(len(convertors)):
+		(version,conversion) = convertors[i]
+		prior_version = (None if i == 0 else convertors[i-1][0])
+		if conversion is None:
+			aliases[-1]['min-version'] = prior_version # last append
+			break
+
+		try:
+			if element != conversion[element]:
+				element = conversion[element] # save the other element
+				aliases.append({'name': element, 'max-version': version})
+		except KeyError as e:
+			if i == 0: raise e # not found in the first version
+			aliases[-1]['min-version'] = prior_version
+			break
+
+	return {
+		'name': material,
+		'aliases': aliases
+	}
+
 def main():
 	versions = get_mc_data_versions()
 	versions.remove('1.7') # old version
@@ -61,14 +89,12 @@ def main():
 		conversion = forward(old, new, legacy_conversion)
 		conversion_queue.append(conversion)
 
-	conversion_queue.append( forward(get_mc_data(versions[-1]), get_spigot_enum_json(), legacy_conversion={}) ) # spigot to Mineflayer 1.20
-	versions.append('Spigot')
+	#conversion_queue.append( forward(get_mc_data(versions[-1]), get_spigot_enum_json(), legacy_conversion={}) ) # spigot to Mineflayer 1.20
+	#versions.append('Spigot')
 
-	element = 'oak_BOAT'.lower()
-	for (version,conversion) in reversed(list(zip(versions,conversion_queue))):
-		print(f"In {version}, it is called {element}")
-		if conversion is not None:
-			element = conversion[element]
+	convertors = list(reversed(list(zip(versions,conversion_queue))))
+	for mat in get_spigot_enum():
+		print(generate_json_entry(mat, convertors))
 
 if __name__ == '__main__':
 	main()
